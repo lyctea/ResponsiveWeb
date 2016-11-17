b6548abe7164999eb42d456820a7c800ef967ad9
lyctea <396136337@qq.com>
2016-11-17 20:51:01 +0800

    添加响应式广告

--- src/js/main.js
+++ src/js/main.js
@@ -5,3 +5,12 @@
 * @Last Modified time: 2016-11-15 15:41:29
 */
 
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+  	items: 1,
+  	loop: true,
+  	autoplay: true,
+  	autoplayTimeout: 1000,
+  	autoplayHoverPause: true
+  });
+});
--- src/js/vender/jquery.js
+++
@@ -1,6 +0,0 @@
-/*
-* @Author: lyctea
-* @Date:   2016-11-15 15:42:56
-* @Last Modified by:   Marte
-* @Last Modified time: 2016-11-15 15:43:08
-*/
